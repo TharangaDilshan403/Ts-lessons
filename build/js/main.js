e3b3914e8ee88a461127ba55f2ded0847bc8ccb5
Tharanga_Dilshan <59679693+TharangaDilshan403@users.noreply.github.com>
2024-07-28 22:31:23 +0530

    lesson-02

--- build/js/main.js
+++ build/js/main.js
@@ -3,4 +3,17 @@ let t = 'Dilshan';
 console.log(t);
 let a = 10;
 let b = 4;
-let c = '2';
+let c = 2;
+let myName = 'tharanga';
+let age = 26;
+let whatCanDo;
+let isLiving = true;
+// this is union data type
+let whatILike;
+//Implicit Type
+let homeTown = 'galle';
+//Explicit Type
+let livingTown = 'colombo';
+const sum = (a, b) => {
+    return a + b;
+};
